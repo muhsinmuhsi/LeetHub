916af69628922ddbc0afebb8fe63159e19075f60
muhsinmuhsi <muhsinmuhsinmuhsi786@gmail.com>
2024-09-26 10:18:25 +0530

    Time: 2142 ms (15.22%), Space: 49 MB (62.06%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var checkPerfectNumber = function(num) {
+    if(num<=1){
+        return false
+    }else{
+        let divisers = []
+    for (let i = 1; i < num; i++) {
+        if (num % i == 0) {
+            divisers.push(i)
+        }
+    }
+
+    const isperect = divisers.reduce((total, currentvalue) => total + currentvalue)
+    return isperect === num
+  }
+}
