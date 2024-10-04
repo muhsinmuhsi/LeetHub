05b604ac6c3a52230a882e4cf939faa7d951a97b
muhsinmuhsi <muhsinmuhsinmuhsi786@gmail.com>
2024-10-04 23:47:03 +0530

    Time: 58 ms (33.69%), Space: 49.7 MB (38.01%) - LeetHub

--- 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.js
+++ 2274-keep-multiplying-found-values-by-two/2274-keep-multiplying-found-values-by-two.js
@@ -7,6 +7,8 @@ var findFinalValue = function(nums, original) {
     for(let i=0;i<nums.length;i++){
       if(nums.includes(original)){
         original=original*2
+      }else{
+        return original
       }
       
    }
