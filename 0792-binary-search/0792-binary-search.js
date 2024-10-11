beccce5cc3901b474db26566b3dddac437bbe329
muhsinmuhsi <muhsinmuhsinmuhsi786@gmail.com>
2024-10-11 09:24:53 +0530

    Time: 56 ms (58.12%), Space: 52.6 MB (34.4%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -4,6 +4,15 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let findindex=nums.indexOf(target)
-   return findindex;
+    let index=[]
+   for(let i=0;i<nums.length;i++){
+      if(nums[i]===target){
+        index.push(i)
+      }
+   }
+   if(index.length>0){
+    return index[0];
+   }else{
+    return -1;
+   }
 };
