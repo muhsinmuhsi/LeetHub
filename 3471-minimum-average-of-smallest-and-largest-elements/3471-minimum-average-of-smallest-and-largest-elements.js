7ffe143c2720f622f86fc88a8ace7c921648f317
muhsinmuhsi <muhsinmuhsinmuhsi786@gmail.com>
2024-11-09 11:09:52 +0530

    Time: 9 ms (8.93%), Space: 51.7 MB (37.1%) - LeetHub

---
+++ 3471-minimum-average-of-smallest-and-largest-elements/3471-minimum-average-of-smallest-and-largest-elements.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumAverage = function(nums) {
+     nums.sort((a,b)=>a-b)
+  let n=nums.length;
+  let minavg=nums[0]+nums[n-1]/2
+  console.log(minavg);
+  for(let i=0;i<n;i++){
+   let avg=(nums[i]+nums[n-1-i]) /2
+   if(avg<minavg){
+      minavg=avg
+   }
+  }
+   return minavg
+};
