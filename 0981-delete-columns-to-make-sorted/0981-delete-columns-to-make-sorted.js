120cc6c5eda43843d7207edc74d78217bcc07144
muhsinmuhsi <muhsinmuhsinmuhsi786@gmail.com>
2025-01-01 10:48:04 +0530

    Time: 71 ms (18.9%), Space: 57.6 MB (18.11%) - LeetHub

---
+++ 0981-delete-columns-to-make-sorted/0981-delete-columns-to-make-sorted.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function(strs) {
+   let count = 0
+    for(let i = 0; i < strs[0].length; i++){
+        let str = ''
+        for(let j = 0; j < strs.length; j++){
+            str += strs[j][i]
+        }
+        let check = str.split('').sort().join('') == str
+        if(!check){
+            count += 1
+        }
+    }
+    return count
+};
